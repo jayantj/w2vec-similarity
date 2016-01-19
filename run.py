e6a52c42cfaeac3e025c96c1e6ca640b145b345f
Jayant Jain <jayantjain1992@gmail.com>
2016-01-19 11:36:25 +0530

    additions to training script

--- run.py
+++ run.py
@@ -2,20 +2,31 @@ from train import train
 import argparse, sys, pdb
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Script to train word2vec models from corpuses or raw files\nRun python run.py --help for help')
+
   parser.add_argument('-f', '--files', nargs='+', help='Input raw text files to be used for training')
+
   parser.add_argument('-c', '--corpuses', nargs='+', help='Input corpuses to be used for training')
   parser.add_argument('-cc', '--components', nargs='+', help='Corpus components to be used for training from given corpus')
   parser.add_argument('-cm', '--components_method', help='Corpus method to be called for components be used for training from given corpus')
-
   parser.add_argument('-s', '--separate', default=False, action='store_true', help='Create separate models for passed corpuses/files')
+
+  parser.add_argument('-o', '--output_file', default='', help='Output file name in which to save the trained model')
+
+  parser.add_argument('-i', '--iter', default=1, type=int, help='Numer of iterations/epochs for the trained model(s)')
+  parser.add_argument('-n', '--size', default=100, type=int, help='Dimensionality of the feature vectors to be trained')
+
   args = parser.parse_args()
+  options = {}
+  for option in ('iter', 'size'):
+    options[option] = getattr(args, option)
+
   if isinstance(args.files, list) and len(args.files):
     pass
   elif isinstance(args.corpuses, list) and len(args.corpuses):
     if args.components_method:
-      train.train_from_corpus_components(args.corpuses[0], args.components_method, args.components)
+      train.train_from_corpus_components(args.corpuses[0], args.components_method, args.components, output_file=args.output_file, separate=args.separate, options=options)
     else:
-      train.train_from_corpuses(args.corpuses)
+      train.train_from_corpuses(args.corpuses, output_file=args.output_file, options=options)
   else:
     parser.print_help()
 
--- train/train.py
+++ train/train.py
@@ -18,17 +18,29 @@ def train_from_corpuses(corpus_names=[], output_file='', options={}):
   for corpus_name in corpus_names:
     corpus = load_corpus(corpus_name)
     all_sents += corpus.sents()
-  output_file = "{0}{1}-model".format(MODELS_DIR, '-'.join(corpus_names)) or output_file
+  output_file = "{0}{1}-{2}-model".format(MODELS_DIR, '-'.join(corpus_names), options_to_string(options)) or output_file
   train_and_save(all_sents, output_file, options)
 
-def train_from_corpus_components(corpus_name, components_method_name='', component_names=[], output_file = '', options={}):
+def train_from_corpus_components(corpus_name, components_method_name='', component_names=[], output_file = '', separate = False, options={}):
   corpus = load_corpus(corpus_name)
   components_method = getattr(corpus, components_method_name)
-  component_names = set(component_names).intersection(components_method())
-  all_sents = corpus.sents(**{components_method_name:component_names})
-  output_file = "{0}{1}-{2}-model".format(MODELS_DIR, corpus_name, '-'.join(component_names)) or output_file
-  train_and_save(all_sents, output_file, options)
+  if component_names:
+    component_names = set(component_names).intersection(components_method())
+  else:
+    component_names = set(components_method())
 
+  if separate:
+    for component_name in component_names:
+      sents = corpus.sents(**{components_method_name:component_name})
+      output_file = "{0}{1}-{2}-{3}-model".format(MODELS_DIR, corpus_name, component_name, options_to_string(options)) or output_file
+      train_and_save(sents, output_file, options)
+  else:
+    all_sents = corpus.sents(**{components_method_name:component_names})
+    output_file = "{0}{1}-{2}-{3}-model".format(MODELS_DIR, corpus_name, '-'.join(component_names), options_to_string(options)) or output_file
+    train_and_save(all_sents, output_file, options)
+
+def options_to_string(options):
+  return '-'.join("{0}-{1}".format(k, v) for k,v in options.iteritems())
 
 
 
