4c0a6cf803c53d0f218bd680088f5f3bee4f21b6
Jayant Jain <jayantjain1992@gmail.com>
2016-01-30 03:45:30 +0530

    train option for different authors

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from train import train
 from glob import glob
-import argparse, sys, pdb
+import argparse, sys, pdb, os
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Script to train word2vec models from corpuses or raw files\nRun python run.py --help for help')
 
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
   parser.add_argument('-i', '--iter', default=1, type=int, help='Numer of iterations/epochs for the trained model(s)')
   parser.add_argument('-n', '--size', default=100, type=int, help='Dimensionality of the feature vectors to be trained')
+  parser.add_argument('-a', '--authors', action='store_true', help='Train separate models for separate authors')
   
   args = parser.parse_args()
   options = {}
@@ -29,6 +30,20 @@ if __name__ == "__main__":
       train.train_from_corpus_components(args.corpuses[0], args.components_method, args.components, output_file=args.output_file, separate=args.separate, options=options)
     else:
       train.train_from_corpuses(args.corpuses, output_file=args.output_file, options=options)
+  elif(args.authors):
+    trained_authors = {}
+    data_dir = 'data/Gutenberg/gutenberg/'
+    for fname in glob(data_dir+'*txt'):
+      author_name = os.path.basename(fname).partition('__')[0]
+      if author_name[0:5] < "Henry":
+        continue
+      if author_name in trained_authors:
+        continue
+      else:
+        trained_authors[author_name] = True
+        output_file = author_name.lower().replace(' ', '_')
+        pattern = data_dir + author_name + '*'
+        train.train_from_files(args.files, args.directory, pattern, False, output_file, options)
   else:
     parser.print_help()
 
