7faf0915f2732be8977e66666d1bdf346e15713f
Jayant Jain <jayantjain1992@gmail.com>
2016-02-26 19:06:23 +0530

    minor changes to evaluate code, adds better visualisation

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,12 +1,17 @@
 from gensim.models import Word2Vec
 from gensim.models import Doc2Vec
-from sklearn.cluster import AffinityPropagation
-from sklearn.manifold import TSNE
+from sklearn.cluster import AffinityPropagation, SpectralClustering
+from sklearn.manifold import TSNE, SpectralEmbedding
 from codecs import open
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
 import random, sys, pdb, cProfile, time, json, os
+import mpld3
+import math
 
+SIMILARITY_DIR = 'models/similarity_matrices/new/'
 def model_similarity(m1, m2, num_chosen_words=100, topn=10):
   m1.init_sims()
   m2.init_sims()
@@ -139,16 +144,17 @@ def save_similarity_matrix(model_files, output_file=''):
       else:
         print "evaluating similarities for models %s, %s" % (model_files[i], model_files[j])
         similarity_matrix[i][j] = 1 - model_similarity(models[i], models[j])
-  output_file = output_file or 'models/similarity_matrices/similarities-%d.json' % time.time()
+  output_file = output_file or (SIMILARITY_DIR + '/similarities-%d.json' % time.time())
   print output_file
   with open(output_file, 'w', 'utf-8') as f:
-    pdb.set_trace()
     json.dump({"model_names":model_files, "similarity_matrix":similarity_matrix}, f)
-  # return output_file
 
-def affinity_propagation_clusters(similarity_matrix, options = {}):
-  pdb.set_trace()
+def affinity_propagation_clusters(similarity_matrix):
   return AffinityPropagation(affinity='precomputed').fit(similarity_matrix)
+
+def spectral_clustering_clusters(similarity_matrix):
+  return SpectralClustering(n_clusters=10, affinity='precomputed').fit(similarity_matrix)
+
 
 def scale_sim_scores(similarity_matrix):
   max_sim_score = 0.0
@@ -165,19 +171,18 @@ def scale_sim_scores(similarity_matrix):
 
 def evaluate_clusters(model_files = [], similarity_file = ''):
   if len(model_files):
-    similarity_file = 'models/similarity_matrices/similarities-%d.json' % time.time()
+    similarity_file = (SIMILARITY_DIR + '/similarities-%d.json' % time.time())
     print similarity_file
-    pdb.set_trace()
     save_similarity_matrix(model_files, similarity_file)
   with open(similarity_file, 'r', 'utf-8') as f:
     similarity_data = json.load(f)
-    afp = affinity_propagation_clusters(scale_sim_scores(similarity_data['similarity_matrix']))
+    cluster_data = spectral_clustering_clusters(scale_sim_scores(similarity_data['similarity_matrix']))
   model_names = similarity_data['model_names']
   doc2cluster = {}
   cluster2doc = {}
   for i in range(len(model_names)):
     model_name = os.path.splitext(os.path.basename(model_names[i]))[0]
-    label = afp.labels_[i]
+    label = str(cluster_data.labels_[i])
     doc2cluster[model_name] = label
     if label in cluster2doc:
       cluster2doc[label].append(model_name)
@@ -185,9 +190,13 @@ def evaluate_clusters(model_files = [], similarity_file = ''):
       cluster2doc[label] = [model_name]
   similarity_data['cluster2doc'] = cluster2doc
   similarity_data['doc2cluster'] = doc2cluster
-  with open(similarity_file, 'w', 'utf-8') as f:
+
+  current_time = time.time()
+  similarity_file_name = os.path.splitext(similarity_file)[0]
+  with open(similarity_file_name + '-%d.json' % current_time, 'w', 'utf-8') as f:
     json.dump(similarity_data, f)
-  with open(similarity_file[0:-5]+'-readable-%d' % time.time(), 'w', 'utf-8') as f:
+
+  with open(similarity_file_name + '-readable-%d' % current_time, 'w', 'utf-8') as f:
     f.write("cluster document mapping\n")
     for key, value in cluster2doc.iteritems():
       f.write(str(key) + "\n\t" + "\n\t".join(sorted(value))+"\n\n")
@@ -201,7 +210,7 @@ def evaluate_clusters(model_files = [], similarity_file = ''):
       sorted_indices = [k[0] for k in sorted(enumerate(row), key=lambda x:-x[1])]
       for index in sorted_indices:
         f.write("\n\t" + str(round(row[index], 3)).ljust(5) + "\t" + os.path.basename(model_names[index]))
-  pdb.set_trace()
+  return similarity_file_name + '-%d.json' % current_time
 
 def evaluate_doc2vec_similarities(doc2vec_file):
   model = Doc2Vec.load(doc2vec_file)
@@ -211,7 +220,6 @@ def evaluate_doc2vec_similarities(doc2vec_file):
   doc_names = model.docvecs.doctags.keys()
   for i, doc1 in enumerate(doc_names):
     for j, doc2 in enumerate(doc_names):
-      # pdb.set_trace()
       if i is j:
         similarity_matrix[i][j] = 1.0
       elif similarity_matrix[j][i]:
@@ -220,29 +228,49 @@ def evaluate_doc2vec_similarities(doc2vec_file):
         doc1_vec = model.docvecs[doc1]
         doc2_vec = model.docvecs[doc2]
         similarity_matrix[i][j] = round((doc1_vec * doc2_vec).sum(),3)
-  with open('models/similarity_matrices/similarity-'+os.path.basename(doc2vec_file), 'w', 'utf-8') as f:
+  with open(SIMILARITY_DIR + '/similarity-' + os.path.basename(doc2vec_file), 'w', 'utf-8') as f:
     json.dump({"model_names":doc_names, "similarity_matrix":similarity_matrix}, f)
 
 def evaluate_doc2vec_clusters(doc2vec_file):
   evaluate_doc2vec_similarities(doc2vec_file)
-  evaluate_clusters(similarity_file='models/similarity_matrices/similarity-'+os.path.basename(doc2vec_file))
+  evaluate_clusters(similarity_file=SIMILARITY_DIR + '/similarity-' + os.path.basename(doc2vec_file))
 
-def tsne_similarity_plot(similarity_file):
+def cluster_scatter_plot(similarity_file):
+  def get_cmap(N):
+    '''Returns a function that maps each index in 0, 1, ... N-1 to a distinct 
+    RGB color.'''
+    color_norm  = colors.Normalize(vmin=0, vmax=N-1)
+    scalar_map = cmx.ScalarMappable(norm=color_norm, cmap='hsv') 
+    def map_index_to_rgb_color(index):
+        return scalar_map.to_rgba(index)
+    return map_index_to_rgb_color
+
   with open(similarity_file, 'r', 'utf-8') as f:
     similarity_data = json.load(f)
   labels = []
+  point_colors = []
+  num_clusters = len(similarity_data['cluster2doc'].keys())
+  cmap = get_cmap(num_clusters)
   for model_name in similarity_data['model_names']:
     model_name = os.path.splitext(os.path.basename(model_name))[0]
     cluster_label = similarity_data['doc2cluster'][model_name]
-    labels.append("%s-%s"%(cluster_label, ''))#model_name.replace('gutenberg-', '')))
-  tsne_embeddings = TSNE(metric='precomputed').fit_transform(np.array(similarity_data['similarity_matrix']))
-  fig = plt.figure(figsize=(15,8))
-  fig.add_subplot(111)
-  plt.scatter(tsne_embeddings[:, 0], tsne_embeddings[:, 1])
-  for label, x, y in zip(labels, tsne_embeddings[:, 0], tsne_embeddings[:, 1]):
-    plt.annotate(
-        label, 
-        xy = (x, y),
-        # textcoords = 'offset points',
-        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5))
-  plt.show()
+    point_colors.append(cmap(cluster_label))
+    labels.append(model_name)#"%s-%s"%(cluster_label, ''))#model_name.replace('gutenberg-', '')))
+  embeddings = SpectralEmbedding(affinity='precomputed').fit_transform(np.array(similarity_data['similarity_matrix']))
+  fig, ax = plt.subplots()
+  x = embeddings[:, 0]
+  y = embeddings[:, 1]
+  annotes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'] * 10
+  N = 100
+  scatter = ax.scatter(x, y, c=point_colors[:],s=100*np.ones(shape=N))
+  tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=labels)
+  mpld3.plugins.connect(fig, tooltip)
+  mpld3.show()
+  # plt.scatter(tsne_embeddings[20:40, 0], tsne_embeddings[20:40, 1], c='b')
+  # for label, x, y in zip(labels, tsne_embeddings[:, 0], tsne_embeddings[:, 1]):
+  #   plt.annotate(
+  #       label, 
+  #       xy = (x, y),
+  #       # textcoords = 'offset points',
+  #       bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5))
+  # plt.show()
